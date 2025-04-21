56b46b5d2538df13174b5e5e45ea6974de7d256e
sage417 <sage417@foxmail.com>
2025-04-21 09:43:14 +0800

    minigrep

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process;
+// crate import
+use minigrep::Config;
+
+// in main.rs
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // let config = Config::new(&args);
+
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // if let match err
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
